64ad98590ebeff935ee58304a8e8b524915a6c1d
Stefan Hagen <stefan@hagen.link>
2023-01-28 21:11:40 +0100

    Bumped impl version for release 2023.1.28 (packaging test) again

--- uuttaminen/__init__.py
+++ uuttaminen/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2023.1.28+parent.f527a215'
-# [[[end]]] (checksum: ea7b5e19e89cdaba6a667f9be2c199e0)
+__version__ = '2023.1.28+parent.4479c01b'
+# [[[end]]] (checksum: c8b416cca66bd2439f169bb8e12cb1a3)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
